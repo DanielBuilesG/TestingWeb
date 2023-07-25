091874cc971f58fdfa1da7cfe99845b0856d0375
DanielBuilesG <daniel.builes99@gmail.com>
2023-07-25 10:46:21 -0500

    mistakes corrected

--- src/test/java/com/mycompany/testingweb/WebLayerTest.java
+++ src/test/java/com/mycompany/testingweb/WebLayerTest.java
@@ -11,10 +11,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
 
 @WebMvcTest(HomeController.class)
